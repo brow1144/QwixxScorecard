fd502d3a1e47575096d64354ddc597f414d519a1
Kyle Brown <brow1144@purdue.edu>
2017-07-05 21:16:18 -0400

    Yellow Completed

--- QwixxScorecard/ViewController.swift
+++ QwixxScorecard/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet var red2: MyButton!
     @IBOutlet var red3: MyButton!
     @IBOutlet var red4: MyButton!
@@ -22,9 +23,25 @@ class ViewController: UIViewController {
     @IBOutlet var red12: MyButton!
     @IBOutlet var redLock: MyButton!
     
-    
     var redsSelected = 0
     @IBOutlet var redTotal: UILabel!
+    
+    @IBOutlet var yellow2: MyButton!
+    @IBOutlet var yellow3: MyButton!
+    @IBOutlet var yellow4: MyButton!
+    @IBOutlet var yellow5: MyButton!
+    @IBOutlet var yellow6: MyButton!
+    @IBOutlet var yellow7: MyButton!
+    @IBOutlet var yellow8: MyButton!
+    @IBOutlet var yellow9: MyButton!
+    @IBOutlet var yellow10: MyButton!
+    @IBOutlet var yellow11: MyButton!
+    @IBOutlet var yellow12: MyButton!
+    
+    @IBOutlet var yellowLock: MyButton!
+    
+    var yellowsSelected = 0
+    @IBOutlet var yellowTotal: UILabel!
     
     
     @IBOutlet var finalScore: UILabel!
@@ -189,6 +206,137 @@ class ViewController: UIViewController {
             redTotal.text = createScore(number: redsSelected)
         }
     }
+    
+    
+    
+    
+    
+    @IBAction func yellow2Pushed(_ sender: Any) {
+        if (yellow2.backgroundColor == UIColor.black) {
+            yellow2.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 102/255, alpha: 1)
+            yellowsSelected -= 1
+            yellowTotal.text = createScore(number: yellowsSelected)
+        } else {
+            yellow2.backgroundColor = UIColor.black
+            yellowsSelected += 1
+            yellowTotal.text = createScore(number: yellowsSelected)
+        }
+    }
+    @IBAction func yellow3Pushed(_ sender: Any) {
+        if (yellow3.backgroundColor == UIColor.black) {
+            yellow3.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 102/255, alpha: 1)
+            yellowsSelected -= 1
+            yellowTotal.text = createScore(number: yellowsSelected)
+        } else {
+            yellow3.backgroundColor = UIColor.black
+            yellowsSelected += 1
+            yellowTotal.text = createScore(number: yellowsSelected)
+        }
+    }
+    @IBAction func yellow4Pushed(_ sender: Any) {
+        if (yellow4.backgroundColor == UIColor.black) {
+            yellow4.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 102/255, alpha: 1)
+            yellowsSelected -= 1
+            yellowTotal.text = createScore(number: yellowsSelected)
+        } else {
+            yellow4.backgroundColor = UIColor.black
+            yellowsSelected += 1
+            yellowTotal.text = createScore(number: yellowsSelected)
+        }
+    }
+    @IBAction func yellow5Pushed(_ sender: Any) {
+        if (yellow5.backgroundColor == UIColor.black) {
+            yellow5.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 102/255, alpha: 1)
+            yellowsSelected -= 1
+            yellowTotal.text = createScore(number: yellowsSelected)
+        } else {
+            yellow5.backgroundColor = UIColor.black
+            yellowsSelected += 1
+            yellowTotal.text = createScore(number: yellowsSelected)
+        }
+    }
+    @IBAction func yellow6Pushed(_ sender: Any) {
+        if (yellow6.backgroundColor == UIColor.black) {
+            yellow6.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 102/255, alpha: 1)
+            yellowsSelected -= 1
+            yellowTotal.text = createScore(number: yellowsSelected)
+        } else {
+            yellow6.backgroundColor = UIColor.black
+            yellowsSelected += 1
+            yellowTotal.text = createScore(number: yellowsSelected)
+        }
+    }
+    @IBAction func yellow7Pushed(_ sender: Any) {
+        if (yellow7.backgroundColor == UIColor.black) {
+            yellow7.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 102/255, alpha: 1)
+            yellowsSelected -= 1
+            yellowTotal.text = createScore(number: yellowsSelected)
+        } else {
+            yellow7.backgroundColor = UIColor.black
+            yellowsSelected += 1
+            yellowTotal.text = createScore(number: yellowsSelected)
+        }
+    }
+    @IBAction func yellow8Pushed(_ sender: Any) {
+        if (yellow8.backgroundColor == UIColor.black) {
+            yellow8.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 102/255, alpha: 1)
+            yellowsSelected -= 1
+            yellowTotal.text = createScore(number: yellowsSelected)
+        } else {
+            yellow8.backgroundColor = UIColor.black
+            yellowsSelected += 1
+            yellowTotal.text = createScore(number: yellowsSelected)
+        }
+    }
+    @IBAction func yelow9Pushed(_ sender: Any) {
+        if (yellow9.backgroundColor == UIColor.black) {
+            yellow9.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 102/255, alpha: 1)
+            yellowsSelected -= 1
+            yellowTotal.text = createScore(number: yellowsSelected)
+        } else {
+            yellow9.backgroundColor = UIColor.black
+            yellowsSelected += 1
+            yellowTotal.text = createScore(number: yellowsSelected)
+        }
+    }
+    @IBAction func yellow10Pushed(_ sender: Any) {
+        if (yellow10.backgroundColor == UIColor.black) {
+            yellow10.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 102/255, alpha: 1)
+            yellowsSelected -= 1
+            yellowTotal.text = createScore(number: yellowsSelected)
+        } else {
+            yellow10.backgroundColor = UIColor.black
+            yellowsSelected += 1
+            yellowTotal.text = createScore(number: yellowsSelected)
+        }
+    }
+    @IBAction func yellow11Pushed(_ sender: Any) {
+        if (yellow10.backgroundColor == UIColor.black) {
+            yellow10.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 102/255, alpha: 1)
+            yellowsSelected -= 1
+            yellowTotal.text = createScore(number: yellowsSelected)
+        } else {
+            yellow10.backgroundColor = UIColor.black
+            yellowsSelected += 1
+            yellowTotal.text = createScore(number: yellowsSelected)
+        }
+    }
+    @IBAction func yellow12Pushed(_ sender: Any) {
+        if (yellow12.backgroundColor == UIColor.black) {
+            yellow12.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 102/255, alpha: 1)
+            yellowLock.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 102/255, alpha: 1)
+            yellowsSelected -= 2
+            yellowTotal.text = createScore(number: yellowsSelected)
+        } else {
+            yellow12.backgroundColor = UIColor.black
+            yellowLock.backgroundColor = UIColor.black
+            yellowsSelected += 2
+            yellowTotal.text = createScore(number: yellowsSelected)
+        }
+    }
+    
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
